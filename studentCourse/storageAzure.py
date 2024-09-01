935851d81064eb589f5670c42c1ac03cdd32decc
c3082560 <c3082560@hallam.shu.ac.uk>
2024-09-02 00:14:05 +0500

    image upload error fix

--- studentCourse/storageAzure.py
+++ studentCourse/storageAzure.py
@@ -4,15 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
 class AzureMediaStorage(AzureStorage):
-    account_name = os.getenv('AZURE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_ACCOUNT_KEY')
+    account_name = 'storagedjango'  # Replace with your Azure account name
+    account_key = 'PJII8m578uyDNPm4mKZTtf+xRAOEgaYPMVksizZIvK/hcavi3cI6KRIX0hyZtpjCwqNhYKc3klCB+AStn4mFmw=='  # Replace with your Azure account key
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = os.getenv('AZURE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_ACCOUNT_KEY')
+    account_name = 'storagedjango'  # Replace with your Azure account name
+    account_key = 'PJII8m578uyDNPm4mKZTtf+xRAOEgaYPMVksizZIvK/hcavi3cI6KRIX0hyZtpjCwqNhYKc3klCB+AStn4mFmw=='  # Replace with your Azure account key
     azure_container = 'static'
     expiration_secs = None
